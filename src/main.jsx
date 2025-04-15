052424a65a9ad3304182b45a4cb07cc3cb60b7dd
Diego Cupri <diegocupri@macbook-pro-de-diego.tail45d3d7.ts.net>
2025-04-15 23:28:40 +0200

    Primer commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import RingrCalculator from './RingrCalculator.jsx'
+import './style.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RingrCalculator />
+  </React.StrictMode>
+)
